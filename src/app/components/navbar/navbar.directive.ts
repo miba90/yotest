f3432d80ed88336fd33c88bc66ec89b074d0e261
miba90 <miba90@gmail.com>
2015-06-27 12:01:30 +0200

    Init commit

---
+++ src/app/components/navbar/navbar.directive.ts
@@ -0,0 +1,28 @@
+module yo1 {
+  'use strict';
+
+  /** @ngInject */
+  export function acmeNavbar(): ng.IDirective {
+
+    return {
+      restrict: 'E',
+      scope: {
+        creationDate: '='
+      },
+      templateUrl: 'app/components/navbar/navbar.html',
+      controller: NavbarController,
+      controllerAs: 'vm',
+      bindToController: true
+    };
+
+  }
+
+  /** @ngInject */
+  class NavbarController {
+    public relativeDate: string;
+
+    constructor(moment: moment.MomentStatic) {
+      this.relativeDate = moment(1435355028240).fromNow();
+    }
+  }
+}
